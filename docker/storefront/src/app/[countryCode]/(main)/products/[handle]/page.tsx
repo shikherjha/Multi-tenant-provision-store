43c9e16f548a3593048ec33d2893394c69154c79
shikherjha <jhasahab1906@gmail.com>
2026-04-14 00:05:30 +0530

    fixing medusa indefinite Next cache & hardcoded HTTP backend public URL

--- docker/storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ docker/storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -119,11 +119,11 @@ export default async function ProductPage(props: Props) {
     queryParams: { handle: params.handle },
   }).then(({ response }) => response.products[0])
 
-  const images = getImagesForVariant(pricedProduct, selectedVariantId)
-
   if (!pricedProduct) {
     notFound()
   }
+
+  const images = getImagesForVariant(pricedProduct, selectedVariantId)
 
   return (
     <ProductTemplate
